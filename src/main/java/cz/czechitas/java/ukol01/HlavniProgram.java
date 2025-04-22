34bfc4f04f5bda5c20b46bfd8e4612c67abafb21
ivaZahor <iva.zahor@gmail.com>
2025-04-22 18:13:47 +0200

    Iva Zahorova DU 1 oprava2 cast

--- src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
+++ src/main/java/cz/czechitas/java/ukol01/HlavniProgram.java
@@ -10,50 +10,101 @@ public class HlavniProgram {
     }
 
     public void start() {
-        zofka = new Turtle();}
+        zofka = new Turtle();
+        zofka.setLocation(500, 500);
+        nakresliPrasatko();
+        zofka.setLocation(200, 700);
+        nakresliOsmiuhelnik();
+        zofka.setLocation(200, 900);
+        nakresliKolecko();
+        zofka.setLocation(200, 200);
+        nakresliSlunicko();
+    }
 
+    public void nakresliPrasatko() {
+        zofka.penDown();
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(100);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(30);
+        zofka.move(100);
+        zofka.turnRight(120);
+        zofka.move(100);
+        zofka.penUp();
+        zofka.turnRight(120);
+        zofka.move(100);
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(50);
+        zofka.turnLeft(135);
+        zofka.penUp();
+        zofka.move(70.7);
+        zofka.turnLeft(135);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.turnRight(135);
+        zofka.move(150);
+        zofka.turnRight(135);
+        zofka.move(50);
+        zofka.turnLeft(135);
+        zofka.penUp();
+        zofka.move(70.7);
+        zofka.turnLeft(135);
+        zofka.penDown();
+        zofka.move(50);
+        zofka.turnLeft(45);
+        zofka.penUp();
+    }
 
-        public void nakresliPrasatko() {
-            zofka.penDown();
-            zofka.move(100);
-            zofka.turnRight(90);
-            zofka.move(150);
-            zofka.turnRight(90);
-            zofka.move(100);
-            zofka.turnRight(90);
-            zofka.move(150);
-            zofka.turnRight(30);
-            zofka.move(100);
-            zofka.turnRight(120);
-            zofka.move(100);
-            zofka.penUp();
-            zofka.turnRight(120);
-            zofka.move(100);
-            zofka.penDown();
+    public void nakresliOsmiuhelnik() {
+        zofka.penDown();
+        for (int i = 0; i < 8; i++) {
+            zofka.move(40);
             zofka.turnRight(45);
-            zofka.move(50);
-            zofka.turnLeft(135);
-            zofka.penUp();
-            zofka.move(70.7);
-            zofka.turnLeft(135);
-            zofka.penDown();
-            zofka.move(50);
-            zofka.turnRight(135);
-            zofka.move(150);
-            zofka.turnRight(135);
-            zofka.move(50);
-            zofka.turnLeft(135);
-            zofka.penUp();
-            zofka.move(70.7);
-            zofka.turnLeft(135);
-            zofka.penDown();
-            zofka.move(50);
-            zofka.turnLeft(45);
-            zofka.penUp();
+        }
+    }
 
-nakresliPrasatko();
+    public void nakresliKolecko() {
+        zofka.penDown();
+        for (int i = 0; i < 12; i++) {
+            zofka.move(30);
+            zofka.turnRight(30);
+        }
+    }
 
-        zofka.move(500);
+    public void nakresliSlunicko() {
+        zofka.penDown();
+           nakresliKolecko();
+           for (int i = 0; i < 12; i++) {
+            zofka.turnLeft(90);
+            zofka.move(40);
+            zofka.turnRight(180);
+            zofka.move(40);
+            zofka.turnLeft(90);
+            zofka.move(30);
+            zofka.turnRight(30);
+        }
+    }
+}
+
+
+    /*public void nakresliDomecek () {
+        zofka.penDown();
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(80);
+        zofka.turnRight(90);
+        zofka.move(150);
+        zofka.turnRight(90);
+        zofka.move(80);
+    }
+
+
+       zofka.move(500);
         zofka.turnRight(90);
         zofka.move(200);
         zofka.penDown();
@@ -137,8 +188,8 @@ nakresliPrasatko();
         zofka.turnRight(30);
         zofka.move(80);
         zofka.turnRight(120);
-       zofka.move(80);
-    }
+        zofka.move(80);
+ */
 
 
 }
